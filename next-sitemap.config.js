56b540d68e91301776265ba82092b3f0bcfd85e8
Manas Madan <manasmadan08@gmail.com>
2023-01-30 02:11:51 +0530

    Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://pdfactions.vercel.app",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  autoLastmod: false,
+};
