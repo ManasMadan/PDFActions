6dc7a2d97eeaf93344f9d7e910a56ecb11463b9c
Manas Madan <manasmadan08@gmail.com>
2024-01-07 00:27:50 +0530

    Footer top layout

---
+++ src/components/DeveloperCard.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DeveloperCard() {
+  return <div className="w-full min-w-[175px]">DeveloperCard</div>;
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,37 @@ import LogoComponent from "./LogoComponent";
 import LanguageSelector from "./LanguageSelector";
 import { getDictionary } from "@/lib/dictionary";
 import CustomLink from "./CustomLinkComponent";
+import DeveloperCard from "./DeveloperCard";
 
 function FooterTop({ lang }) {
-  return <section className="container">Footer Top</section>;
+  return (
+    <section className="container grid gap-x-3 py-3 xl:grid-cols-2">
+      <div>
+        <h1 className="text-lg">About Us</h1>
+        <p className="text-sm text-[#00000080]">
+          Welcome to PDFActions - your secure, hassle-free PDF solution. Process
+          files in your browser with no data uploads. Works on Windows, Mac,
+          Linux, and mobile devices. Simplify your PDF tasks with us!
+        </p>
+      </div>
+      <div className="flex flex-col sm:flex-row">
+        <div className="flex w-full max-w-[650px] flex-col sm:flex-row">
+          <DeveloperCard />
+          <DeveloperCard />
+        </div>
+        <div className="w-full sm:max-w-[300px] lg:max-w-none">
+          <h1>Powered By</h1>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 async function FooterBottom({ lang }) {
   const { home_page } = await getDictionary(lang);
   return (
     <section className="bg-primary text-white">
-      <div className="container grid grid-cols-1 items-center py-3 sm:grid-cols-1 lg:grid-cols-3 lg:py-6">
+      <div className="container grid grid-cols-1 items-center py-3 lg:grid-cols-3 lg:py-6">
         <div className="flex items-center justify-between">
           <CustomLink
             className="text-[#D3D3D3CC] lg:text-lg"
