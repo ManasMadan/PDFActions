7910cd776896f3133da9b65a565b0c49e3209c54
Manas Madan <manasmadan08@gmail.com>
2024-01-07 19:11:13 +0530

    PDF Tools Card Hover effect

--- src/components/PDFToolCard.jsx
+++ src/components/PDFToolCard.jsx
@@ -1,16 +1,24 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import styles from "@/styles/pdftoolcard.module.css";
+import CustomLink from "./CustomLinkComponent";
 
 export default function PDFToolCard({ details }) {
   return (
-    <Link href={details.link}>
-      <div className="mx-auto w-full max-w-[300px] rounded-2xl bg-primary p-1 text-white">
-        <div className="flex flex-col items-center gap-4 rounded-xl border-2 border-white py-6">
-          <Image src={details.icon} width={32} height={32} />
+    <CustomLink href={details.link}>
+      <div
+        className={cn(
+          "mx-auto w-full max-w-[300px] rounded-2xl bg-primary p-1 text-white",
+          styles.cardwrapper,
+        )}
+      >
+        <div className="flex flex-col items-center gap-4 rounded-xl border-2 border-white py-6 transition-all">
+          <Image src={details.icon} width={32} height={32} alt={details.text} />
           <p className="text-xl">{details.text}</p>
         </div>
       </div>
-    </Link>
+    </CustomLink>
   );
 }
