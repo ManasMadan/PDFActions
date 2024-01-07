d03c2db20aff8b41f0b4e1e4d3efda8144618bc8
Manas Madan <manasmadan08@gmail.com>
2024-01-07 22:14:45 +0530

    Navbar

--- src/app/[lang]/page.js
+++ src/app/[lang]/page.js
@@ -1,9 +1,11 @@
+import LandingSection from "@/components/LandingSection";
 import PDFTools from "@/components/PDFTools";
 import WhyUse from "@/components/WhyUse";
 
 export default async function Home({ params: { lang } }) {
   return (
     <>
+      <LandingSection />
       <PDFTools lang={lang} />
       <WhyUse lang={lang} />
     </>
---
+++ src/components/LandingSection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+export default function LandingSection() {
+  return (
+    <section className="bg-primary">
+      <Navbar />
+    </section>
+  );
+}
