7fe53232887ce4dc71a2a5f8df43dc9d3070a03a
Manas Madan <manasmadan08@gmail.com>
2024-01-09 21:41:31 +0530

    Landing Page Background SVG

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import styles from "@/styles/landingsection.module.css";
 
 export default function LandingSection() {
   return (
     <section className="bg-primary">
       <Navbar />
+      <div className={styles.spacer}></div>
     </section>
   );
 }
