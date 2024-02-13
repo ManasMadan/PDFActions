c10288d1f78039178c078fb4a6815578132eacac
Manas Madan <manasmadan08@gmail.com>
2024-02-14 02:31:32 +0530

    Added a Modal for revamp website

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import Head from "next/head";
 import { PDFIcon } from "../components/icons";
 import Link from "next/link";
 
 export default function Home() {
+  const modalRef = useRef(null);
   const allPDFTools = [
     ["Merge PDF", "/pdf-tools/merge"],
     ["Split PDF", "/pdf-tools/split"],
@@ -17,6 +18,9 @@ export default function Home() {
     ["Edit PDF Metadata", "/pdf-tools/editmetadata"],
     ["Remove PDF Metadata", "/pdf-tools/removemetadata"],
   ];
+  const closeModal = () => {
+    modalRef.current.classList.add("hidden");
+  };
   return (
     <>
       <Head>
@@ -30,6 +34,52 @@ export default function Home() {
           <div className="absolute" style={{ transform: "rotate(10deg)" }}>
             <PDFIcon width="100" />
           </div>
+        </div>
+      </div>
+      <div
+        ref={modalRef}
+        className="absolute z-50 left-1/2 top-1/2 bg-amber-200 border-black border-2 px-8 py-4"
+        style={{ transform: "translate(-50%,-50%)" }}
+      >
+        <span className="text-blue-800">
+          <Link href="https://www.linkedin.com/in/manas-madan/">
+            Manas Madan
+          </Link>
+        </span>{" "}
+        and{" "}
+        <span className="text-blue-800">
+          <Link href="https://www.linkedin.com/in/ayush-arora4/">
+            Ayush Arora
+          </Link>
+        </span>{" "}
+        are revamping this website
+        <div className="mt-2">
+          Check out the new look on{" "}
+          <span className="text-blue-800">
+            <Link href="https://pdfactions-git-revamp-manasmadan.vercel.app/">
+              https://pdfactions-git-revamp-manasmadan.vercel.app/
+            </Link>
+          </span>
+        </div>
+        <div className="mt-2">
+          Mail out any suggestions to{" "}
+          <span className="text-blue-800">
+            <Link href="mailto:manasmadan08@gmail.com">
+              manasmadan08@gmail.com
+            </Link>
+          </span>{" "}
+          or{" "}
+          <span className="text-blue-800">
+            <Link href="mailto:ayusharora4102004@gmail.com">
+              ayusharora4102004@gmail.com
+            </Link>
+          </span>
+        </div>
+        <div
+          onClick={closeModal}
+          className="mt-4 bg-blue-800 text-white px-4 py-2 w-fit mx-auto cursor-pointer"
+        >
+          Close
         </div>
       </div>
       <div className="flex items-center justify-center flex-wrap gap-2 mb-12">
